9f2c0128e2f42ed25b5346f25fa1845595ffc641
Austin Hodak <ahodak65@gmail.com>
2022-04-02 18:54:58 -0400

    Organize schema.js

--- schema.js
+++ schema.js
@@ -1,277 +1,276 @@
 module.exports = `
-  enum ItemType {
-    ammo
-    ammoBox
-    any
-    armor
-    backpack
-    barter
-    container
-    disabled
-    glasses
-    grenade
-    gun
-    headphones
-    helmet
-    injectors
-    keys
-    markedOnly
-    meds
-    mods
-    noFlea
-    pistolGrip
-    preset
-    provisions
-    rig
-    suppressor
-    unLootable
-    wearable
-  }
-
-  enum TraderName {
-    prapor
-    therapist
-    fence
-    skier
-    peacekeeper
-    mechanic
-    ragman
-    jaeger
-  }
-
-  enum ItemSourceName {
-    prapor
-    therapist
-    fence
-    skier
-    peacekeeper
-    mechanic
-    ragman
-    jaeger
-    fleaMarket
-  }
-
-  enum RequirementType {
-      playerLevel
-      loyaltyLevel
-      questCompleted
-  }
-
-  enum StatusCode {
-      OK
-      Updating
-      Unstable
-      Down
-  }
-
-  enum LanguageCode {
-      en
-  }
-
-  type GameProperty {
-    key: String!
-    numericValue: Float
-    stringValue: String
-    arrayValue: [String]
-    objectValue: String
-  }
-
-  type Item {
-    id: ID!
-    name: String
-    normalizedName: String
-    shortName: String
-    basePrice: Int!
-    updated: String
-    width: Int!
-    height: Int!
-    iconLink: String
-    iconLinkFallback: String!
-    wikiLink: String
-    imageLink: String
-    imageLinkFallback: String!
-    gridImageLink: String
-    gridImageLinkFallback: String!
-    types: [ItemType]!
-    avg24hPrice: Int
-    accuracyModifier: Float
-    recoilModifier: Float
-    ergonomicsModifier: Float
-    hasGrid: Boolean
-    blocksHeadphones: Boolean
-    traderPrices: [TraderPrice]!
-    link: String
-    lastLowPrice: Int
-    changeLast48h: Float
-    changeLast48hPercent: Float
-    low24hPrice: Int
-    high24hPrice: Int
-    sellFor: [ItemPrice!]
-    buyFor: [ItemPrice!]
-    containsItems: [ContainedItem]
-    bsgCategoryId: String
-    weight: Float
-    velocity: Float
-    loudness: Int
-
-    translation(languageCode: LanguageCode): ItemTranslation
-  }
-
-  type ItemPrice {
-      source: ItemSourceName
-      price: Int
-      currency: String
-      requirements: [PriceRequirement]!
-  }
-
-  type ItemTranslation {
-      name: String
-      shortName: String
-      description: String
-  }
-
-  type historicalPricePoint {
-    price: Int
-    timestamp: String
-  }
-
-  type PriceRequirement {
-      type: RequirementType
-      value: Int
-  }
-
-  type ItemAttribute {
-    type: String!
-    value: String
-  }
-
-  type ContainedItem {
-    item: Item!
-    count: Float!
-    quantity: Float!
-    attributes: [ItemAttribute]
-  }
-
-  type Barter {
-    source: String!
-    sourceName: ItemSourceName!
-    requiredItems: [ContainedItem]!
-    rewardItems: [ContainedItem]!
-    requirements: [PriceRequirement]!
-  }
-
-  type Craft {
-    source: String!
-    duration: Int!
-    requiredItems: [ContainedItem]!
-    rewardItems: [ContainedItem]!
-  }
-
-  type Trader {
-    id: String!
-    name: String!
-  }
-
-  type TraderInventory {
-    id: String!
-    name: TraderName!
-    items: [TraderInventoryItem!]
-  }
-
-  type TraderInventoryItem {
-    item: Item!
-    minLevel: Int
-    price: Int
-    updated: String
-    questUnlockId: String
-    currency: String
-  }
-
-  type QuestRequirement {
-      level: Int
-      quests: [[Int]]!
-      prerequisiteQuests: [[Quest]]!
-  }
-
-  type QuestRewardReputation {
-      trader: Trader!
-      amount: Float!
-  }
-
-  type QuestObjective {
-      id: String
-      type: String!
-      target: [String!]
-      targetItem: Item
-      number: Int
-      location: String
-  }
-
-  type Quest {
-    id: String!
-    requirements: QuestRequirement
-    giver: Trader!
-    turnin: Trader!
-    title: String!
-    wikiLink: String!
-    exp: Int!
-    unlocks: [String]!
-    reputation: [QuestRewardReputation!]
-    objectives: [QuestObjective]!
-  }
-
-  type TraderPrice {
-      price: Int!
-      trader: Trader!
-  }
-
-  type HideoutModule {
-      id: Int
-      name: String
-      level: Int
-      itemRequirements: [ContainedItem]!
-      moduleRequirements: [HideoutModule]!
-  }
-
-  type Status {
-      name: String!
-      message: String
-      status: Int!
-      statusCode: String!
-  }
-
-  type StatusMessage {
-    content: String!
-    time: String!
-    type: Int!
-    solveTime: String
-    statusCode: String!
+type Query {
+  item(id: ID!): Item
+  itemsByType(type: ItemType!): [Item]!
+  itemsByName(name: String!): [Item]!
+  itemByNormalizedName(normalizedName: String!): Item
+  itemsByBsgCategoryId(bsgCategoryId: String!): [Item]!
+  historicalItemPrices(id: ID!): [historicalPricePoint]!
+  barters: [Barter]
+  crafts: [Craft]
+  quests: [Quest]
+  hideoutModules: [HideoutModule]
+  status: ServerStatus!
+  # traderInventoryByName(name: TraderName!): TraderInventory
+  traderResetTimes: [TraderResetTime]
 }
 
-  type ServerStatus {
-      generalStatus: Status
-      currentStatuses: [Status]
-      messages: [StatusMessage]
-  }
+type Barter {
+  source: String!
+  sourceName: ItemSourceName!
+  requiredItems: [ContainedItem]!
+  rewardItems: [ContainedItem]!
+  requirements: [PriceRequirement]!
+}
 
-  type TraderResetTime {
-      name: String
-      resetTimestamp: String
-  }
+type ContainedItem {
+  item: Item!
+  count: Float!
+  quantity: Float!
+  attributes: [ItemAttribute]
+}
 
-  type Query {
-    item(id: ID!): Item
-    itemsByType(type: ItemType!): [Item]!
-    itemsByName(name: String!): [Item]!
-    itemByNormalizedName(normalizedName: String!): Item
-    itemsByBsgCategoryId(bsgCategoryId: String!): [Item]!
-    historicalItemPrices(id: ID!): [historicalPricePoint]!
-    barters: [Barter]
-    crafts: [Craft]
-    quests: [Quest]
-    hideoutModules: [HideoutModule]
-    status: ServerStatus!
-    # traderInventoryByName(name: TraderName!): TraderInventory
-    traderResetTimes: [TraderResetTime]
-  }
+type Craft {
+  source: String!
+  duration: Int!
+  requiredItems: [ContainedItem]!
+  rewardItems: [ContainedItem]!
+}
+
+type GameProperty {
+  key: String!
+  numericValue: Float
+  stringValue: String
+  arrayValue: [String]
+  objectValue: String
+}
+
+type HideoutModule {
+  id: Int
+  name: String
+  level: Int
+  itemRequirements: [ContainedItem]!
+  moduleRequirements: [HideoutModule]!
+}
+
+type historicalPricePoint {
+  price: Int
+  timestamp: String
+}
+
+type Item {
+  id: ID!
+  name: String
+  normalizedName: String
+  shortName: String
+  basePrice: Int!
+  updated: String
+  width: Int!
+  height: Int!
+  iconLink: String
+  iconLinkFallback: String!
+  wikiLink: String
+  imageLink: String
+  imageLinkFallback: String!
+  gridImageLink: String
+  gridImageLinkFallback: String!
+  types: [ItemType]!
+  avg24hPrice: Int
+  accuracyModifier: Float
+  recoilModifier: Float
+  ergonomicsModifier: Float
+  hasGrid: Boolean
+  blocksHeadphones: Boolean
+  traderPrices: [TraderPrice]!
+  link: String
+  lastLowPrice: Int
+  changeLast48h: Float
+  changeLast48hPercent: Float
+  low24hPrice: Int
+  high24hPrice: Int
+  sellFor: [ItemPrice!]
+  buyFor: [ItemPrice!]
+  containsItems: [ContainedItem]
+  bsgCategoryId: String
+  weight: Float
+  velocity: Float
+  loudness: Int
+  translation(languageCode: LanguageCode): ItemTranslation
+}
+
+enum ItemType {
+  ammo
+  ammoBox
+  any
+  armor
+  backpack
+  barter
+  container
+  disabled
+  glasses
+  grenade
+  gun
+  headphones
+  helmet
+  injectors
+  keys
+  markedOnly
+  meds
+  mods
+  noFlea
+  pistolGrip
+  preset
+  provisions
+  rig
+  suppressor
+  unLootable
+  wearable
+}
+
+type ItemAttribute {
+  type: String!
+  value: String
+}
+
+type ItemPrice {
+  source: ItemSourceName
+  price: Int
+  currency: String
+  requirements: [PriceRequirement]!
+}
+
+enum ItemSourceName {
+  prapor
+  therapist
+  fence
+  skier
+  peacekeeper
+  mechanic
+  ragman
+  jaeger
+  fleaMarket
+}
+
+type ItemTranslation {
+  name: String
+  shortName: String
+  description: String
+}
+
+enum LanguageCode {
+  en
+}
+
+type PriceRequirement {
+  type: RequirementType
+  value: Int
+}
+
+type Quest {
+  id: String!
+  requirements: QuestRequirement
+  giver: Trader!
+  turnin: Trader!
+  title: String!
+  wikiLink: String!
+  exp: Int!
+  unlocks: [String]!
+  reputation: [QuestRewardReputation!]
+  objectives: [QuestObjective]!
+}
+
+type QuestObjective {
+  id: String
+  type: String!
+  target: [String!]
+  targetItem: Item
+  number: Int
+  location: String
+}
+
+type QuestRequirement {
+  level: Int
+  quests: [[Int]]!
+  prerequisiteQuests: [[Quest]]!
+}
+
+type QuestRewardReputation {
+  trader: Trader!
+  amount: Float!
+}
+
+enum RequirementType {
+  playerLevel
+  loyaltyLevel
+  questCompleted
+}
+
+type Status {
+  name: String!
+  message: String
+  status: Int!
+  statusCode: String!
+}
+
+enum StatusCode {
+  OK
+  Updating
+  Unstable
+  Down
+}
+
+type StatusMessage {
+  content: String!
+  time: String!
+  type: Int!
+  solveTime: String
+  statusCode: String!
+}
+
+type ServerStatus {
+  generalStatus: Status
+  currentStatuses: [Status]
+  messages: [StatusMessage]
+}
+
+type Trader {
+  id: String!
+  name: String!
+}
+
+type TraderInventory {
+  id: String!
+  name: TraderName!
+  items: [TraderInventoryItem!]
+}
+
+type TraderInventoryItem {
+  item: Item!
+  minLevel: Int
+  price: Int
+  updated: String
+  questUnlockId: String
+  currency: String
+}
+
+enum TraderName {
+  prapor
+  therapist
+  fence
+  skier
+  peacekeeper
+  mechanic
+  ragman
+  jaeger
+}
+
+type TraderPrice {
+  price: Int!
+  trader: Trader!
+}
+
+type TraderResetTime {
+  name: String
+  resetTimestamp: String
+}
 `;
