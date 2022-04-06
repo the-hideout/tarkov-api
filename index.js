d7b279d5e14d56107ecbe5949ccd44076d61ffc2
Razzmatazz <razz@badkarma.xyz>
2022-04-06 17:38:44 -0500

    possibly fix TypeError

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ async function sha256(message) {
     const msgBuffer = new TextEncoder().encode(message);
 
     // hash the message
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+    const hashBuffer = await crypto.webcrypto.subtle.digest('SHA-256', msgBuffer);
 
     // convert ArrayBuffer to Array
     const hashArray = Array.from(new Uint8Array(hashBuffer));
