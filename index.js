2504a49c2a74292656e6c81fc42f59193e15295a
Grant Birkinbine <grantbirki@github.com>
2022-06-11 23:36:29 -0600

    use cache service

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //const crypto = require('crypto');
-const {EventEmitter} = require('events');
+const { EventEmitter } = require('events');
 
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 const { mergeTypeDefs } = require('@graphql-tools/merge');
@@ -19,6 +19,15 @@ const graphqlUtil = require('./utils/graphql-util');
 const nightbot = require('./custom-endpoints/nightbot');
 const twitch = require('./custom-endpoints/twitch');
 
+// cache url
+const cacheUrl = 'https://cache.tarkov.dev'
+// default headers
+const headers = {
+    headers: {
+        'content-type': 'application/json;charset=UTF-8',
+    }
+};
+
 let schema = false;
 let loadingSchema = false;
 //const schemaEvents = new EventEmitter();
@@ -28,8 +37,79 @@ let loadingSchema = false;
  * Example of how router can be used in an application
  *  */
 
+// Helper function to create a hash from a string
+// :param string: string to hash
+// :return: SHA-256 hash of string
+async function hash(string) {
+    const utf8 = new TextEncoder().encode(string);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', utf8);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+        .map((bytes) => bytes.toString(16).padStart(2, '0'))
+        .join('');
+
+    console.log(hashHex);
+    return hashHex;
+}
+
+// Updates the cache with the results of a query
+// :param json: the incoming request in json
+// :param body: the body to cache
+// :return: true if successful, false if not
+async function updateCache(json, body) {
+    try {
+        // Get the cacheKey from the request
+        const query = json.query.trim();
+        const cacheKey = await hash(query);
+
+        // headers and POST body
+        const headersPost = {
+            body: JSON.stringify({ key: cacheKey, value: body }),
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json;charset=UTF-8',
+            },
+        };
+
+        // Update the cache
+        const response = await fetch(`${cacheUrl}/api/cache`, headersPost);
+
+        // Log non-200 responses
+        if (response.status !== 200) {
+            console.log(`failed to write to cache: ${response.status}`);
+            return false
+        }
+
+        return true
+    } catch (error) {
+        console.log('updateCache error: ' + error.message);
+        return false;
+    }
+}
+
+// Checks the caching service to see if a request has been cached
+// :param json: the json payload of the incoming worker request
+// :return: json results of the item found in the cache or false if not found
+async function checkCache(json) {
+    try {
+        const query = json.query.trim();
+        const cacheKey = await hash(query);
+
+        const response = await fetch(`${cacheUrl}/api/cache?key=${cacheKey}`, headers);
+        if (response.status === 200) {
+            const results = JSON.stringify(await response.json());
+            return results
+        }
+
+        return false
+    } catch (error) {
+        console.log('checkCache error: ' + error.message);
+        return false;
+    }
+}
+
 async function getSchema() {
-    if (schema){
+    if (schema) {
         return schema;
     }
     if (loadingSchema) {
@@ -51,32 +131,32 @@ async function getSchema() {
     }
     loadingSchema = true;
     return dynamicTypeDefs(dataAPI).then(dynamicTypeDefs => {
-            schema = makeExecutableSchema({typeDefs: mergeTypeDefs([typeDefs, dynamicTypeDefs]), resolvers: resolvers});
-            loadingSchema = false;
-            //schemaEvents.emit('loaded');
-            return schema;
-        }).catch(error => {
-            loadingSchema = false;
-            return Promise.reject(error);
-        });
+        schema = makeExecutableSchema({ typeDefs: mergeTypeDefs([typeDefs, dynamicTypeDefs]), resolvers: resolvers });
+        loadingSchema = false;
+        //schemaEvents.emit('loaded');
+        return schema;
+    }).catch(error => {
+        loadingSchema = false;
+        return Promise.reject(error);
+    });
 }
 
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event));
 });
 
-async function graphqlHandler(event, graphQLOptions) {
+async function graphqlHandler(event, graphQLOptions, json) {
     const request = event.request;
     const url = new URL(request.url);
     let query = false;
     let variables = false;
 
-    if(request.method === 'POST'){
+    if (request.method === 'POST') {
         try {
             const requestBody = await request.json();
             query = requestBody.query;
             variables = requestBody.variables;
-        } catch (jsonError){
+        } catch (jsonError) {
             console.error(jsonError);
 
             return new Response(null, {
@@ -85,7 +165,7 @@ async function graphqlHandler(event, graphQLOptions) {
         }
     }
 
-    if(request.method === 'GET'){
+    if (request.method === 'GET') {
         query = url.searchParams.get('query');
         variables = url.searchParams.get('variables');
     }
@@ -110,8 +190,11 @@ async function graphqlHandler(event, graphQLOptions) {
     } */
 
     await dataAPI.init();
-    const result = await graphql(await getSchema(), query, {}, {data: dataAPI, util: graphqlUtil}, variables);
+    const result = await graphql(await getSchema(), query, {}, { data: dataAPI, util: graphqlUtil }, variables);
     const body = JSON.stringify(result);
+
+    // Update the cache with the results of the query
+    await updateCache(json, body);
 
     /* if(!result.errors && !url.hostname.includes('localhost') && !url.hostname.includes('tutorial.cloudflareworkers.com')){
         await QUERY_CACHE.put(queryHash, body, {expirationTtl: 300});
@@ -141,7 +224,7 @@ const graphQLOptions = {
     // Enable CORS headers on GraphQL requests
     // Set to `true` for defaults (see `utils/setCors`),
     // or pass an object to configure each header
-  //   cors: true,
+    //   cors: true,
     cors: {
         allowCredentials: 'true',
         allowHeaders: 'Content-type',
@@ -159,10 +242,10 @@ const graphQLOptions = {
 const handleRequest = async event => {
     const request = event.request;
     const url = new URL(request.url);
+    const json = await request.clone().json();
 
     // Check for empty /graphql query
     if (url.pathname === "/graphql" && request.method === 'POST') {
-        const json = await request.clone().json();
         if (json.query.trim() === "") {
             // Clone the response so that it's no longer immutable
             const response = new Response('GraphQL requires a query in the body of the request', { status: 200 });
@@ -172,20 +255,26 @@ const handleRequest = async event => {
         }
     }
 
+    // Check the cache service for data first - If cached data exists, return it
+    const cachedResponse = await checkCache(json);
+    if (cachedResponse) {
+        return new Response(cachedResponse, headers);
+    }
+
     try {
-        if(url.pathname === '/webhook/nightbot'){
+        if (url.pathname === '/webhook/nightbot') {
             return nightbot(request, dataAPI);
         }
 
-        if(url.pathname === '/webhook/stream-elements'){
+        if (url.pathname === '/webhook/stream-elements') {
             return nightbot(request, dataAPI);
         }
 
-        if(url.pathname === '/webhook/moobot'){
+        if (url.pathname === '/webhook/moobot') {
             return nightbot(request, dataAPI);
         }
 
-        if(url.pathname === '/twitch'){
+        if (url.pathname === '/twitch') {
             const response = request.method === 'OPTIONS' ? new Response('', { status: 204 }) : await twitch(request);
             if (graphQLOptions.cors) {
                 setCors(response, graphQLOptions.cors);
@@ -195,7 +284,7 @@ const handleRequest = async event => {
         }
 
         if (url.pathname === graphQLOptions.baseEndpoint) {
-            const response = request.method === 'OPTIONS' ? new Response('', { status: 204 }) : await graphqlHandler(event, graphQLOptions);
+            const response = request.method === 'OPTIONS' ? new Response('', { status: 204 }) : await graphqlHandler(event, graphQLOptions, json);
             if (graphQLOptions.cors) {
                 setCors(response, graphQLOptions.cors);
             }
@@ -203,7 +292,7 @@ const handleRequest = async event => {
             return response;
         }
 
-        if ( graphQLOptions.playgroundEndpoint && url.pathname === graphQLOptions.playgroundEndpoint ) {
+        if (graphQLOptions.playgroundEndpoint && url.pathname === graphQLOptions.playgroundEndpoint) {
             return playground(request, graphQLOptions);
         }
 
