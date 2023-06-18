6c95ce3280bfab75ef28ab31ba4851a6b86b0522
grantbirki <grantbirki@github.com>
2023-06-17 21:29:36 -0600

    twitch fixes for ci

--- custom-endpoints/twitch.js
+++ custom-endpoints/twitch.js
@@ -1,10 +1,21 @@
+var twitch_token_value = '';
+var twitch_client_id_value = '';
+try {
+    // this value comes from our wrangler environment variables as a secret
+    twitch_token_value = TWITCH_TOKEN
+    twitch_client_id_value = TWITCH_CLIENT_ID
+} catch (e) {
+    console.warn('TWITCH_TOKEN or TWITCH_CLIENT_ID not set; twitch features disabled');
+}
+
+
 // The Twitch API url to hit for EFT data
 const url = "https://api.twitch.tv/helix/streams?game_id=491931&first=100"
 // Twitch API headers for authentication
 const init = {
     headers: {
-        'Authorization': `Bearer ${TWITCH_TOKEN}`,
-        'Client-ID': TWITCH_CLIENT_ID,
+        'Authorization': `Bearer ${twitch_token_value}`,
+        'Client-ID': twitch_client_id_value,
     },
 };
 // Response content-type headers
