aed621a556f00edc85d01a99712506e712cd5cac
Razzmatazz <razz@badkarma.xyz>
2025-05-28 21:59:17 -0500

    increase max age of options response

--- index.mjs
+++ index.mjs
@@ -204,7 +204,7 @@ export default {
             const optionsResponse = new Response(null, {
                 headers: {
                     'cache-control': 'public, max-age=2592000',
-                    'Access-Control-Max-Age': '600',
+                    'Access-Control-Max-Age': '86400',
                 },
             });
             setCors(optionsResponse, graphQLOptions.cors);
