2645d550fd88ae38535d9c979b59a4accdf5357a
Razzmatazz <razz@badkarma.xyz>
2025-12-28 09:26:10 -0600

    add debugging output

--- http/env-binding.mjs
+++ http/env-binding.mjs
@@ -24,7 +24,7 @@ async function messageParentProcess(message) {
         const messageId = uuidv4();
         const responseTimeout = setTimeout(() => {
             emitter.off(messageId, messageResponseHandler);
-            reject(new Error('Response from primary process timed out'));
+            reject(new Error(`Response from primary process timed out for: ${JSON.stringify(message)}`));
         }, message.timeout ?? 10000);
         const messageResponseHandler = (response) => {
             clearTimeout(responseTimeout);
