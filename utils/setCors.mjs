0727b46b1dbd277e662fa5e5aaf87825cfbdd74d
Razzmatazz <razz@badkarma.xyz>
2026-06-23 15:49:16 -0500

    use graphql yoga in worker

--- utils/setCors.mjs
+++ utils/setCors.mjs
@@ -1,22 +1,26 @@
 import graphQLOptions from "./graphql-options.mjs";
 
 const setCorsHeaders = (response, config) => {
-  const corsConfig = config instanceof Object ? config : graphQLOptions.cors;
+    const corsConfig = config instanceof Object ? config : graphQLOptions.cors;
 
-  response.headers.set(
-    'Access-Control-Allow-Credentials',
-    corsConfig?.allowCredentials ?? 'true',
-  )
-  response.headers.set(
-    'Access-Control-Allow-Headers',
-    corsConfig?.allowHeaders ?? 'Content-type',
-  )
-  response.headers.set(
-    'Access-Control-Allow-Methods',
-    corsConfig?.allowMethods ?? 'OPTIONS, GET, POST',
-  )
-  response.headers.set('Access-Control-Allow-Origin', corsConfig?.allowOrigin ?? '*')
-  response.headers.set('X-Content-Type-Options', 'nosniff')
+    try {
+        response.headers.set(
+            'Access-Control-Allow-Credentials',
+            corsConfig?.allowCredentials ?? 'true',
+        );
+        response.headers.set(
+            'Access-Control-Allow-Headers',
+            corsConfig?.allowHeaders ?? 'Content-type',
+        );
+        response.headers.set(
+            'Access-Control-Allow-Methods',
+            corsConfig?.allowMethods ?? 'OPTIONS, GET, POST',
+        );
+        response.headers.set('Access-Control-Allow-Origin', corsConfig?.allowOrigin ?? '*');
+        response.headers.set('X-Content-Type-Options', 'nosniff');
+    } catch (error) {
+        console.log('Could not modify immutable headers');
+    }
 }
 
 export default setCorsHeaders;
